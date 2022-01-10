9f60f6c45ded412de4a2a9db6cc8afa5cc870ff0
Christopher Cole <ccole@well.ox.ac.uk>
2022-01-10 07:49:01 -0500

    Add tag functionality and navbar

--- cookbook/app.py
+++ cookbook/app.py
@@ -8,6 +8,23 @@ import glob
 def list_recipes():
     return [a.replace("recipes/", "").replace(".yaml", "") for a in glob.glob('recipes/*.yaml') if a != 'recipes/template.yaml']
 
+def list_all_in_category(category: str): 
+    for r in list_recipes():
+        ro = yaml.safe_load(open(f"recipes/{r}.yaml")) 
+        if category in ro["categories"]:
+            yield r
+
+def list_all_categories():
+    recipes = list_recipes()
+    categories = []
+    for ro in recipes:
+        r = yaml.safe_load(open(f"recipes/{ro}.yaml"))
+        for category in r["categories"]:
+            if category not in categories:
+                categories.append(category)
+
+    return categories
+
 # Create the blueprint
 cookbook = Blueprint('cookbook', __name__, template_folder='templates')
 
@@ -15,12 +32,33 @@ cookbook = Blueprint('cookbook', __name__, template_folder='templates')
 def index():
     return render_template('index.html', recipes=list_recipes())
 
+@cookbook.route('/recipes.html')
+def recipe_homepage():
+    return render_template('recipes.html', recipes=list_recipes())
+
+
+@cookbook.route('/category/<category>')
+def category(category):
+    return render_template('category.html', category=category, recipes=[a for a in list_all_in_category(category)])
+
+
 @cookbook.route('/recipe/<recipe_id>.html')
 def recipe(recipe_id):
     with open(f"recipes/{recipe_id}.yaml") as f:
         recipe_data = yaml.safe_load(f)
     
     return render_template('recipe.html', recipe=recipe_data)
+
+@cookbook.route('/tags.html')
+
+
+def recipe_tags():
+    recipes_in_category = {}
+    for category in list_all_categories():
+        recipes_in_category[category] = [a for a in list_all_in_category(category)]
+    
+    return render_template('tags.html', categories=recipes_in_category)
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
